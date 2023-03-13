70eb8877f5eed6652ebad0629be5a1b4300a75a1
ASUS\Homer_Chen <Homer_Chen@asus.com>
2023-03-13 15:24:07 +0800

    [+]add CommonController [*]update gitignore

---
+++ EMP.Website/Controllers/CommonController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UniversalLibrary.Models;
+
+namespace EMP.Website.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+
+    public class CommonController : ControllerBase
+    {
+        /// <summary>
+        /// 取得UserName
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet()]
+        public async Task<Result<string>> GetUserName()
+        {
+            var result = new Result<string>() { Success = false };
+            try
+            {
+                result.Content = User.Identity.Name.Split('\\')[1];
+                result.Success = true;
+            }
+            catch (System.Exception ex)
+            {
+                result.Message = ex.Message;
+            }
+
+            return result;
+        }
+    }
+}
